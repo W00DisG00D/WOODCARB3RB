fd08e1038ec6ec0ea503f2c0b47bc6b4707f00bf
Alan T. Arnholt <arnholtat@appstate.edu>
2017-09-25 11:22:21 -0400

    minor tweaks

--- tests/testthat/testVars.R
+++ tests/testthat/testVars.R
@@ -1,4 +1,4 @@
-library(WOODCARB3R)
+library(WOODCARB3RB)
 context("Test final variables.")
 
 #table of final variables to test
@@ -6,7 +6,7 @@ finalvars <- finalVariables()
 
 #read in correct table to test against
 check <- system.file("extdata/CopyOfData",
-                     "finalVariablesCheck.csv",package="WOODCARB3R")
+                     "finalVariablesCheck.csv",package="WOODCARB3RB")
 check <- read.csv(check)
 
 test_that("check variables", {
